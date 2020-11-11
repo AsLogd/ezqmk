7f1e73d166632448898447c6c13228efeece1c4a
aslogd <gerarddelcastillo@gmail.com>
2020-11-11 13:15:54 +0100

    fix permissions and git modules

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ echo "[EZQMK] Clonning qmk_firmware..."
 git clone https://github.com/qmk/qmk_firmware
 cd qmk_firmware
 echo "[EZQMK] Git submodules..."
-git submodules
+make git-submodule
 echo "[EZQMK] Installing dependencies..."
-pacman --noconfirm --needed --disable-download-timeout -S avr-gcc avr-libc python python-pip python-setuptools libffi
+sudo pacman --noconfirm --needed --disable-download-timeout -S avr-gcc avr-libc python python-pip python-setuptools libffi
 echo "[EZQMK] Installing python requirements..."
-python -m pip install requirements.txt
+sudo python -m pip install -r requirements.txt
 echo "[EZQMK] Finished"
